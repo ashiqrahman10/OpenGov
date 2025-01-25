d5da2a06d0287021adb37a8c56da26e22c730124
Amjedusman <amjadusman98@gmail.com>
2025-01-25 23:27:06 +0530

    login and register added

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -55,7 +55,13 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+		fontFamily: {
+			poppins: ['Poppins', 'sans-serif'],
+			inter: ['Inter', 'sans-serif'],
+			gloock: ['Gloock', 'serif'],
+			press: ['"Press Start 2P"', 'cursive'],
+		},
   	}
   },
   plugins: [require("tailwindcss-animate")],
